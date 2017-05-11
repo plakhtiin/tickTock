b7325bd199fc66bc7905ff8833765aa228e6387c
Anton Vakulenko <anton.vakulenko93@gmail.com>
2017-05-11 22:47:44 +0300

    add test connection

--- api/index.js
+++ api/index.js
@@ -20,148 +20,180 @@ db.connectToServer(function (err) {
 	}
 });
 var app = express();
+var exports = module.exports = {};
 var port = process.env.PORT || 8888;
 var io = socketIo.listen(server);
 server.createServer(app);
-var listener = app.listen(port, function () {
-	console.log('Server listening at port ' + listener.address().port);
-});
-app.use(bodyParser.json())
-	.get('/', function (req, res, next) {
-		res.sendFile(__dirname.replace('/api', '') + '/front/app/index.html');
-	})
-	.use(express.static(__dirname.replace('/api', '') + '/front/app'))
-	.post("/api/login", function (req, res) {
-		loginService.getUser(req.body.username, req.body.password, function (err, adminUser) {
-			if (err) {
-				res.send(err);
-			}
-			else {
-				setToken(adminUser, function (error, result) {
-					if (error) {
-						res.send(error);
-					} else if (result) {
-						var obj = {
-							userData: adminUser,
-							result: result
-						};
-						res.send(obj);
-					}
-				});
-			}
-		});
-	})
-	.get("/api/user/data/:userId/:token", function (req, res) {
-		loginService.isValidToken(req.params.token, function (isValid) {
-			if (isValid) {
-				manageUserService.getUserData(req.params.userId, function (err, usersDays) {
-					if (err) {
-						res.send(err);
-					}
-					else {
-						res.send(usersDays);
-					}
-				});
-			}
-			else {
-				res.status(403).send('Error');
-			}
-		});
-	})
-	.get("/api/users/data/:token", function (req, res) {
-		loginService.isValidToken(req.params.token, function (isValid) {
-			if (isValid) {
-				manageUserService.getUsers(function (err, users) {
-					if (err) {
-						res.send(err);
-					}
-					else {
-						res.send(users);
-					}
-				});
-			}
-			else {
-				res.status(403).send('Error');
-			}
-		});
-	})
-	.post('/api/updateuser/data/:token', function (req, res) {
-		loginService.isValidToken(req.params.token, function (isValid) {
-			if (isValid) {
-				manageUserService.updateUser(req.body, function (err, user) {
-					if (err) {
-						res.send(err);
-					}
-					else {
-						res.send(user);
-					}
-				});
-			}
-			else {
-				res.status(403).send('Error');
-			}
-		});
-	})
-	.post('/api/createuser/data/:token', function (req, res) {
-		loginService.isValidToken(req.params.token, function (isValid) {
-			if (isValid) {
-				manageUserService.createUser(req.body, function (err, user) {
-					if (err) {
-						res.send(err);
-					}
-					else {
-						res.send(user);
-					}
-				});
-			}
-			else {
-				res.status(403).send('Error');
-			}
-		});
-	})
-	.post('/api/removeuser/data/:token', function (req, res) {
-		loginService.isValidToken(req.params.token, function (isValid) {
-			if (isValid) {
-				manageUserService.removeUser(req.body, function (err, user) {
-					if (err) {
-						res.send(err);
-					}
-					else {
-						res.send(user);
-					}
-				});
-			}
-			else {
-				res.status(403).send('Error');
-			}
-		});
-	})
-	.post('/api/schedule/start/:userId/:token', function (req, res) {
-		loginService.isValidToken(req.params.token, function (isValid) {
-			if (isValid) {
-				timeService.startTime(req.params.userId, function (err, startTimeId) {
-					if (err) {
-						// res.send(err);
-						res.status(404).send('Error');
-					}
-					else {
-						console.log(startTimeId);
-						res.send(startTimeId);
-					}
-				});
-			}
-			else {
-				res.status(403).send('Error');
-			}
-		});
-	});
 
-function setToken(userData, cb) {
-	db.setToken(userData.id, userData.token, function (err, res) {
-		if (err) {
-			cb(err, null);
-		} else if (res) {
-			cb(null, res);
-		}
-	})
-}
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+var connectionTester = require('connection-tester');
+
+
+// if (cluster.isMaster) {
+//     console.log(numCPUs)
+//     for (var i = 0; i < numCPUs; i++) {
+//         // Create a worker
+//         cluster.fork();
+//     }
+// } else {
+    // Workers share the TCP connection in this server
+    // var app = express();
+    // app.get('/', function (req, res) {
+    //     res.send('Hello World!');
+    // });
+    // All workers use this port
+    var listener = app.listen(port, function () {
+        console.log('Server listening at port ' + listener.address().port);
+        var j =0;
+        for(var i=0; i<200; i++) {
+            connectionTester.test('localhost', 8888, 500, function (err, output) {
+                console.log(output, j);
+                j++;
+            });
+        }
+    });
+    app.use(bodyParser.json())
+        .get('/', function (req, res, next) {
+            res.sendFile(__dirname.replace('/api', '') + '/front/app/index.html');
+        })
+        .use(express.static(__dirname.replace('/api', '') + '/front/app'))
+        .post("/api/login", function (req, res) {
+            loginService.getUser(req.body.username, req.body.password, function (err, adminUser) {
+                if (err) {
+                    res.send(err);
+                }
+                else {
+                    setToken(adminUser, function (error, result) {
+                        if (error) {
+                            res.send(error);
+                        } else if (result) {
+                            var obj = {
+                                userData: adminUser,
+                                result: result
+                            };
+                            res.send(obj);
+                        }
+                    });
+                }
+            });
+        })
+        .get("/api/user/data/:userId/:token", function (req, res) {
+            loginService.isValidToken(req.params.token, function (isValid) {
+                if (isValid) {
+                    manageUserService.getUserData(req.params.userId, function (err, usersDays) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        else {
+                            res.send(usersDays);
+                        }
+                    });
+                }
+                else {
+                    res.status(403).send('Error');
+                }
+            });
+        })
+        .get("/api/users/data/:token", function (req, res) {
+            loginService.isValidToken(req.params.token, function (isValid) {
+                if (isValid) {
+                    manageUserService.getUsers(function (err, users) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        else {
+                            res.send(users);
+                        }
+                    });
+                }
+                else {
+                    res.status(403).send('Error');
+                }
+            });
+        })
+        .post('/api/updateuser/data/:token', function (req, res) {
+            loginService.isValidToken(req.params.token, function (isValid) {
+                if (isValid) {
+                    manageUserService.updateUser(req.body, function (err, user) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        else {
+                            res.send(user);
+                        }
+                    });
+                }
+                else {
+                    res.status(403).send('Error');
+                }
+            });
+        })
+        .post('/api/createuser/data/:token', function (req, res) {
+            loginService.isValidToken(req.params.token, function (isValid) {
+                if (isValid) {
+                    manageUserService.createUser(req.body, function (err, user) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        else {
+                            res.send(user);
+                        }
+                    });
+                }
+                else {
+                    res.status(403).send('Error');
+                }
+            });
+        })
+        .post('/api/removeuser/data/:token', function (req, res) {
+            loginService.isValidToken(req.params.token, function (isValid) {
+                if (isValid) {
+                    manageUserService.removeUser(req.body, function (err, user) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        else {
+                            res.send(user);
+                        }
+                    });
+                }
+                else {
+                    res.status(403).send('Error');
+                }
+            });
+        })
+        .post('/api/schedule/start/:userId/:token', function (req, res) {
+            loginService.isValidToken(req.params.token, function (isValid) {
+                if (isValid) {
+                    timeService.startTime(req.params.userId, function (err, startTimeId) {
+                        if (err) {
+                            // res.send(err);
+                            res.status(404).send('Error');
+                        }
+                        else {
+                            console.log(startTimeId);
+                            res.send(startTimeId);
+                        }
+                    });
+                }
+                else {
+                    res.status(403).send('Error');
+                }
+            });
+        });
+
+    function setToken(userData, cb) {
+        db.setToken(userData.id, userData.token, function (err, res) {
+            if (err) {
+                cb(err, null);
+            } else if (res) {
+                cb(null, res);
+            }
+        })
+    }
+// }
+
+
+
+
